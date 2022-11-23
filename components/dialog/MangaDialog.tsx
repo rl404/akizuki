1c6370bd8aac34d281c12576d3ddcde7006e3431
axel <axel.rl.404@gmail.com>
2022-11-23 16:25:48 +0700

    update: reverse score dropdown options

--- components/dialog/MangaDialog.tsx
+++ components/dialog/MangaDialog.tsx
@@ -340,7 +340,7 @@ const MangaDialog = ({
             </Grid>
             <Grid item xs={showManga ? false : 12} sm={showManga ? false : 6}>
               <TextField select label="Score" value={userScore} onChange={onChangeUserScore} size="small" fullWidth>
-                {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((s) => {
+                {[10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0].map((s) => {
                   return (
                     <MenuItem value={s} key={s}>
                       {s}
--- components/dialog/ScoreDialog.tsx
+++ components/dialog/ScoreDialog.tsx
@@ -102,7 +102,7 @@ const ScoreDialog = ({
           </Stack>
         ) : (
           <TextField select label="Score" fullWidth value={newScore} onChange={onChangeScore} size="small">
-            {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((s) => {
+            {[10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0].map((s) => {
               return (
                 <MenuItem value={s} key={s}>
                   {s}
