93035040acf612850babac0b51865020fc32bdc7
axel <axel.rl.404@gmail.com>
2023-01-22 11:13:29 +0700

    update: enable robots.txt

--- components/head/Head.tsx
+++ components/head/Head.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 
 const AkizukiHead = ({ title = 'Home' }) => {
   const description = 'View and edit your anime and manga list in a more modern design with custom tags editor.';
-  const image = '';
+  const image = 'https://akizuki.rl404.com/images/edit-form-3.jpg';
   return (
     <Head>
       <title>{`${title} | Akizuki`}</title>
@@ -14,9 +14,9 @@ const AkizukiHead = ({ title = 'Home' }) => {
       <meta property="og:type" content="website" />
       <meta property="og:site_name" content={process.env.NEXT_PUBLIC_SITE_NAME} />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
-      <meta name="keyword" content="" />
-      <meta name="robots" content="noindex" />
+      <meta name="robots" content="index, nofollow" />
       <meta httpEquiv="Content-Type" content="text/html charset=utf-8" />
+      <meta charSet="UTF-8" />
     </Head>
   );
 };
